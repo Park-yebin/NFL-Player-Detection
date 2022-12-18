db083bbfb2befe58946a9ed9a9543c811d39762a
Park-yebin <81156273+Park-yebin@users.noreply.github.com>
2022-12-18 13:48:23 +0900

    Create Code.py
    select the subject and code from

---
+++ Code.py
@@ -0,0 +1,18 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.14.4
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# #### Code from Kaggle competition: 1st and Future-Player Contact Detection, Most voted 'NFL Player Contact Detection-Simple XGB baseline'
+
+
